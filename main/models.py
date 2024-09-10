a7c158fc59f3b3dec4cbf5f4b2535e714b249ad1
safirasalmahumaira <safiramulianto@gmail.com>
2024-09-10 11:36:45 +0700

    main

--- main/models.py
+++ main/models.py
@@ -1,11 +1,9 @@
 from django.db import models
 
-class MoodEntry(models.Model):
-    name = models.CharField(max_length=255)
+class Item(models.Model):
+    name = models.CharField(max_length=200)
     price = models.IntegerField()
-    description = models.TextField(max_length=2000)
+    description = models.TextField()
 
-
-    @property
-    def is_mood_strong(self):
-        return self.mood_intensity > 5
+    def __str__(self):
+        return self.name
--- main/views.py
+++ main/views.py
@@ -8,7 +8,7 @@ def show_main(request):
         'Class': 'PBP F',
         'Products': [
             {'Name': 'Moisturizer', 'Price': 250.000, 'Description': 'This lightweight moisturizer is specially formulated for oily skin. It helps to control excess sebum production without drying out your skin. The non-greasy formula provides hydration while maintaining a shine-free finish. Perfect for those seeking a balanced and healthy complexion.'},
-            {'name': 'Facial Wash', 'Price': 150.000, 'Description': 'Our facial wash is specially formulated to combat excess oil and shine. It gently cleanses pores, removing impurities and preventing breakouts. With its refreshing formula, your skin will feel clean, mattified, and balanced.'},
+            {'Name': 'Facial Wash', 'Price': 150.000, 'Description': 'Our facial wash is specially formulated to combat excess oil and shine. It gently cleanses pores, removing impurities and preventing breakouts. With its refreshing formula, your skin will feel clean, mattified, and balanced.'},
             # Add more items as needed
         ],
     }

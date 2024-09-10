7667da5d22a69a924d672a253b43d2d0f3561baa
safirasalmahumaira <safiramulianto@gmail.com>
2024-09-10 11:38:47 +0700

    main edit

--- main/views.py
+++ main/views.py
@@ -6,7 +6,7 @@ def show_main(request):
         'Name' : 'Safira Salma Humaira',
         'NPM' : '2306245850',
         'Class': 'PBP F',
-        'Products': [
+        'products': [
             {'Name': 'Moisturizer', 'Price': 250.000, 'Description': 'This lightweight moisturizer is specially formulated for oily skin. It helps to control excess sebum production without drying out your skin. The non-greasy formula provides hydration while maintaining a shine-free finish. Perfect for those seeking a balanced and healthy complexion.'},
             {'Name': 'Facial Wash', 'Price': 150.000, 'Description': 'Our facial wash is specially formulated to combat excess oil and shine. It gently cleanses pores, removing impurities and preventing breakouts. With its refreshing formula, your skin will feel clean, mattified, and balanced.'},
             # Add more items as needed

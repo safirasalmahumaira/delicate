7a70260ba54efaa1a14d119ba780999135bda430
safirasalmahumaira <safiramulianto@gmail.com>
2024-09-10 12:32:57 +0700

    views, main

--- main/views.py
+++ main/views.py
@@ -3,14 +3,22 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
-        'Name' : 'Safira Salma Humaira',
-        'NPM' : '2306245850',
-        'Class': 'PBP F',
-        'products': [
-            {'Name': 'Moisturizer', 'Price': 250.000, 'Description': 'This lightweight moisturizer is specially formulated for oily skin. It helps to control excess sebum production without drying out your skin. The non-greasy formula provides hydration while maintaining a shine-free finish. Perfect for those seeking a balanced and healthy complexion.'},
-            {'Name': 'Facial Wash', 'Price': 150.000, 'Description': 'Our facial wash is specially formulated to combat excess oil and shine. It gently cleanses pores, removing impurities and preventing breakouts. With its refreshing formula, your skin will feel clean, mattified, and balanced.'},
-            # Add more items as needed
-        ],
-    }
+    'name': 'Safira Salma Humaira',
+    'class': 'PBP F',
+    'NPM': '2306245850',
+    'products': [
+        {
+            'Name': 'Moisturizer',
+            'Price': '250.000',
+            'Description': 'This lightweight moisturizer is specially formulated for oily skin. It helps to control excess sebum production without drying out your skin. The non-greasy formula provides hydration while maintaining a shine-free finish. Perfect for those seeking a balanced and healthy complexion.'
+        },
+        {
+            'Name': 'Facial Wash',
+            'Price': '150.000',
+            'Description': 'Our facial wash is specially formulated to combat excess oil and shine. It gently cleanses pores, removing impurities while preserving the skin’s natural moisture barrier.'
+        }
+    ]
+}
+
 
     return render(request, "main.html", context)

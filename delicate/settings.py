7bddcd78b24a9cce61569d8d97e3a410d2c49404
safirasalmahumaira <safiramulianto@gmail.com>
2024-09-23 20:41:20 +0700

    settings py

--- delicate/settings.py
+++ delicate/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(j&)4=5(-w6-n$-93de19&g2-sh5&el_m@yzv1&flx!*)6t7oo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-PRODUCTION = os.getenv("PRODUCTION", False)
+PRODUCTION = os.getenv("PRODUCTION", True)
 DEBUG = not PRODUCTION
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "safira-salma-d.pbp.cs.ui.ac.id"]

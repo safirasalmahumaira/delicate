02505810376a50843e2af787156a58a056dc1bc4
safirasalmahumaira <safiramulianto@gmail.com>
2024-10-07 20:53:16 +0700

    tugas 6

--- main/urls.py
+++ main/urls.py
@@ -6,6 +6,7 @@ from main.views import login_user
 from main.views import logout_user
 from main.views import edit_item
 from main.views import delete_item
+from main.views import add_item_entry_ajax
 
 app_name = 'main'  # Gunakan namespace 'main'
 
@@ -24,4 +25,5 @@ urlpatterns = [
     path('logout/', logout_user, name='logout'),
     path('edit-mood/<uuid:id>', edit_item, name='edit_item'),
     path('delete/<uuid:id>', delete_item, name='delete_item'),
+    path('create-mood-entry-ajax', add_item_entry_ajax, name='add_item_entry_ajax'),
 ]
--- main/views.py
+++ main/views.py
@@ -12,15 +12,17 @@ from django.contrib.auth.decorators import login_required
 import datetime
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+from django.utils.html import strip_tags
 
 @login_required(login_url='/login')
 def show_main(request):
-    item_entries = Item.objects.filter(user=request.user)
+    item_entries = Item.objects.filter(user=request.user)  # Ambil data produk
     context = {
         'name': 'Safira Salma Humaira',  # Key is 'name'
         'class': 'PBP F',                # Key is 'class'
         'NPM': '2306245850',
-        'item_entries': item_entries,
         'last_login': request.COOKIES['last_login'],
     }
     return render(request, 'main.html', context)
@@ -50,11 +52,11 @@ def create_item_entry(request):
     return render(request, "create_item_entry.html", context)
 
 def show_xml(request):
-    data = Item.objects.all()
+    data = Item.objects.filter(user=request.user)
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
 
 def show_json(request):
-    data = Item.objects.all()
+    data = Item.objects.filter(user=request.user)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
 def show_xml_by_id(request, item_id):
@@ -87,6 +89,8 @@ def login_user(request):
             response = HttpResponseRedirect(reverse("main:show_main"))
             response.set_cookie('last_login', str(datetime.datetime.now()))
             return response
+      else:
+            messages.error(request, "Invalid username or password. Please try again.")
 
    else:
       form = AuthenticationForm(request)
@@ -114,3 +118,21 @@ def delete_item(request, id):
     item = Item.objects.get(pk = id)
     item.delete()
     return HttpResponseRedirect(reverse('main:show_main'))
+
+@csrf_exempt
+@require_POST
+def add_item_entry_ajax(request):
+    name = strip_tags(request.POST.get("name")) # strip HTML tags!
+    price = strip_tags(request.POST.get("price")) # strip HTML tags!
+    description = strip_tags(request.POST.get("description")) # strip HTML tags!
+    user = request.user
+
+    new_item = ItemEntry(
+        name=name, price=price,
+        description=description,
+        user=user
+    )
+    new_item.save()
+
+    return HttpResponse(b"CREATED", status=201)
+
